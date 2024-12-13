123af61214c9ce633b264930b5491b6dae3228c8
Alex Miller <fotoflo@gmail.com>
2024-12-13 19:39:01 +0700

    add gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,29 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import "../styles/globals.css"; // Add this line
+import { useEffect } from "react";
+import Script from "next/script";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Initialize gtag
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", process.env.NEXT_PUBLIC_GTAG);
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
+      <Script
+        id="gtag"
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG}`}
+      />
       <Component {...pageProps} />
     </QueryClientProvider>
   );
