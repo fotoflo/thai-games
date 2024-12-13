f9938896eb70e9d17a9d9e51c8e8990f1257eac2
Alex Miller <fotoflo@gmail.com>
2024-12-13 19:48:42 +0700

    change _app from tsx to jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,17 +1,16 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import type { AppProps } from "next/app";
 import "../styles/globals.css"; // Add this line
 import { useEffect } from "react";
 import Script from "next/script";
 
 const queryClient = new QueryClient();
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   useEffect(() => {
     // Initialize gtag
     window.dataLayer = window.dataLayer || [];
     function gtag() {
-      dataLayer.push(arguments);
+      window.dataLayer.push(arguments);
     }
     gtag("js", new Date());
     gtag("config", process.env.NEXT_PUBLIC_GTAG);
